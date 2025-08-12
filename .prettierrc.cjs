ee3be5f22454af55c38a499d429194628d0109bc
jerrylingj <498539230@qq.com>
2025-08-12 23:08:59 +0800

    feat(init): 项目初始化, 配置eslint, prettier, commitlint, husky

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+// .prettierrc.cjs
+module.exports = {
+  semi: true, // 行尾需要分号
+  singleQuote: true, // 使用单引号
+  trailingComma: 'all', // 结尾需要逗号
+  printWidth: 80, // 单行代码最大长度
+  tabWidth: 2, // tab 宽度为2个空格
+};
